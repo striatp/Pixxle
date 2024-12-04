ed8c065e139b5c4d0c3015c701682ac929c6872a
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-04 19:51:13 +0100

    Create level_view.js

---
+++ Source/Applications/level/level_view.js
@@ -0,0 +1,16 @@
+module.exports = {
+    data: {
+        type: 1,
+        name: "view",
+        description: "Shows yours or another member's leveling progression.",
+        options: [
+            {
+                type: 6,
+                name: "member",
+                description: "Select a member to view their leveling progression.",
+                required: false
+            }
+        ]
+    },
+    code: ``
+}
