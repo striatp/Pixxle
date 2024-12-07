3cd06db6aa783d1d58505c8a39d6982f8f8b2a81
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-07 12:06:00 +0100

    Update level_view.js

--- Source/Applications/level/level_view.js
+++ Source/Applications/level/level_view.js
@@ -1,5 +1,5 @@
 const config = require("../../config.js");
-const primary = `${config.COLOR.primary}`;
+const primary = `${config.COLORS.primary}`;
 
 module.exports = {
     data: {
@@ -16,21 +16,24 @@ module.exports = {
         ]
     },
     code: `
+$c[Local variables definition]
 $let[User;$if[$option[member]==;$authorID;$option[member]]]
-
 $let[Level;$getUserVar[$guildID-Level;$get[User];0]]
 $let[XP;$getUserVar[$guildID-XP;$get[User];0]]
 $let[ReqXP;$getUserVar[$guildID-ReqXP;$get[User];100]]
-
 $let[Avatar;$userAvatar[$get[User]]]
 $let[Username;$username[$get[User]]]
 
-$let[Image;https://api.aggelos-007.xyz/rankcard?username=$get[Username]&xp=$get[XP]&maxxp=$get[ReqXP]&level=$get[Level]&avatar=$get[Avatar]]
+$c[Generating the card]
+$let[Image;https://api.aggelos-007.xyz/rankcard?username=$get[Username]&xp=$get[XP]&maxxp=$get[ReqXP]&level=$get[Level]&avatar=$get[Avatar]&xpcolor=ff3c65]
 
-$image[$get[Image]]
-$color[${primary}]
+$c[Outputting the embed]
+$interactionReply[
+    $image[$get[Image]]
+    $color[${primary}]
 
-$addActionRow
-  $addButton[$authorID-Edit-Level-Card;Edit Background;Secondary;🪄]
+    $addActionRow
+      $addButton[$authorID-Edit-Level-Card;Edit Background;Secondary;🪄]
+]
 `
 }
