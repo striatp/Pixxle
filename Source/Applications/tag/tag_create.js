7b69111a71cd868529e1f65f87fb8933b3dc776e
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-14 20:38:51 +0100

    Update tag_create.js

--- Source/Applications/tag/tag_create.js
+++ Source/Applications/tag/tag_create.js
@@ -8,20 +8,20 @@ module.exports = {
         description: "Create a new tag to store reusable content.",
         options: [
             {
-                type: 3,
+                type: 3, // String option type
                 name: "name",
                 description: "The unique name for the tag (2-30 characters).",
                 required: true,
-                max_length: 30,
-                min_length: 2
+                max_length: 30, // String maximum length
+                min_length: 2 // String minimal length
             },
             {
-                type: 3,
+                type: 3, // String option type
                 name: "content",
                 description: "The content of the tag (1-1800 characters).",
                 required: true,
-                max_length: 1800,
-                min_length: 1
+                max_length: 1800, // String maximum length
+                min_length: 1 // String minimal length
             }
         ]
     },
