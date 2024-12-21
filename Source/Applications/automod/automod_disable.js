0545ed33ba215f1076abb309335ff5411155a709
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-21 22:04:17 +0100

    Create automod_disable.js

---
+++ Source/Applications/automod/automod_disable.js
@@ -0,0 +1,11 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "disable",
+        description: "Disable the Auto-Mod system in the server."
+    },
+    code: ``
+}
