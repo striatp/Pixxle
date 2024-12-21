0ad31cc1c9aff84d05da7cbe218723c6258280ac
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-21 22:05:03 +0100

    Create automod_settings.js

---
+++ Source/Applications/automod/automod_settings.js
@@ -0,0 +1,11 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "settings",
+        description: "View current Auto-Mod settings, such as enabled filters and thresholds."
+    },
+    code: ``
+}
