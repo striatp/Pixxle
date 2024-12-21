db0621d966d4b6ac2a79384b70b959661a460ea4
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-21 22:06:00 +0100

    Create automod_set.js

---
+++ Source/Applications/automod/automod_set.js
@@ -0,0 +1,11 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "set",
+        description: "Configure specific Auto-Mod options."
+    },
+    code: ``
+}
