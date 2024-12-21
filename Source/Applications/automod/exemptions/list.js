99ed20b24bf6e4072e9bbef4a285a01969460ce5
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-21 23:47:39 +0100

    Create list.js

---
+++ Source/Applications/automod/exemptions/list.js
@@ -0,0 +1,11 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "list",
+        description: "List roles exempt from Auto-Mod actions."
+    },
+    code: ``
+}
