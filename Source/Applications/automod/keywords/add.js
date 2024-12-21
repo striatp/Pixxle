6ffc0c6b01b96baf9c7be4e616e1be6127b47d5d
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-21 23:53:07 +0100

    Create add.js

---
+++ Source/Applications/automod/keywords/add.js
@@ -0,0 +1,23 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "add",
+        description: "The keyword to block.",
+        options: [
+            {
+                type: 3,
+                name: "keyword",
+                description: "The keyword to block.",
+                required: true
+            },
+            {
+                type: 3,
+                name: "
+            }
+        ]
+    },
+    code: ``
+}
