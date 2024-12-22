3c97bb32e00e3c2faf0a6f7566d0221069a58955
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-22 15:01:52 +0100

    Create automod_heatmap.js

---
+++ Source/Applications/automod/automod_heatmap.js
@@ -0,0 +1,11 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "heatmap",
+        description: "Generate a heatmap showing server activity and flagged violations."
+    },
+    code: ``
+}
