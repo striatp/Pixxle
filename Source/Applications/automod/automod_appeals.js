fa0fd40170de24872a87107f522e8de362c5de25
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-22 12:43:26 +0100

    Create automod_appeals.js

---
+++ Source/Applications/automod/automod_appeals.js
@@ -0,0 +1,11 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "appeals",
+        description: "View and manage appeals from flagged users."
+    },
+    code: ``
+}
