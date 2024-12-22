45f3f7553796a070b98b8a08a0cb1173a9418e1e
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-22 19:25:16 +0100

    Update automod_disable.js

--- Source/Applications/automod/automod_disable.js
+++ Source/Applications/automod/automod_disable.js
@@ -7,5 +7,26 @@ module.exports = {
         name: "disable",
         description: "Disable the Auto-Mod system in the server."
     },
-    code: ``
+    code: `
+$onlyIf[$guildOwnerID[$guildId]==$authorID;$interactionReply[
+    $ephemeral
+    $description[Only the server owner can disable Auto-Mod features.]
+    $color[${primary_color}]
+]]
+
+$let[AutoMod;$getGuildVar[AutoMod;$guildID;false]]
+$onlyIf[$get[AutoMod]==true;$interactionReply[
+    $ephemeral
+    $description[Auto-Mod features are already disabled.]
+    $color[${primary_color}]
+]]
+
+$setGuildVar[AutoMod;false;$guildID]
+
+$interactionReply[
+    $ephemeral
+    $description[Auto-Mod features have been disabled. (Old settings are saved)]
+    $color[${primary_color}]
+]
+`
 }
