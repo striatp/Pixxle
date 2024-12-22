0488f040e03ddf967328467c1c4a494d2c89af2a
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-22 13:05:28 +0100

    Create disable.js

---
+++ Source/Applications/automod/raid/disable.js
@@ -0,0 +1,11 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "disable",
+        description: "Disable anti-raid protection."
+    },
+    code: ``
+}
