d3e10b97dbdb0d2f913b53841be44431a3b3e228
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-22 12:36:11 +0100

    Create block.js

---
+++ Source/Applications/automod/links/block.js
@@ -0,0 +1,19 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "remove",
+        description: "Block specific domains from being shared.",
+        options: [
+            {
+                type: 3,
+                name: "domain",
+                description: "The domain to block.",
+                required: true
+            }
+        ]
+    },
+    code: ``
+}
