c1153a06048439a4e870cb4493971adb1764d494
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-22 16:21:25 +0100

    Create automod_reset.js

---
+++ Source/Applications/automod/automod_reset.js
@@ -0,0 +1,11 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "reset",
+        description: "Reset all Auto-Mod settings to default."
+    },
+    code: ``
+}
