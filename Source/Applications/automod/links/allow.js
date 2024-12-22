70a3b3f34b0da6968da244fe1e7e1e8ec6feb29d
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-22 12:35:32 +0100

    Create allow.js

---
+++ Source/Applications/automod/links/allow.js
@@ -0,0 +1,19 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "allow",
+        description: "Add domains to the allowlist for link sharing.",
+        options: [
+            {
+                type: 3,
+                name: "domain",
+                description: "The domain to allow.",
+                required: true
+            }
+        ]
+    },
+    code: ``
+}
