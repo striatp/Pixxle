726dbc09bbe6a4aa9195cdfb815338f0509d5071
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-22 16:21:48 +0100

    Create automod_logs.js

---
+++ Source/Applications/automod/automod_logs.js
@@ -0,0 +1,11 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "logs",
+        description: "View the bot’s Auto-Mod action logs."
+    },
+    code: ``
+}
