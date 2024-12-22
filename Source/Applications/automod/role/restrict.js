ddd973f34d0a262d1a3e534bdae93c1d16f963b2
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-22 14:57:15 +0100

    Create restrict.js

---
+++ Source/Applications/automod/role/restrict.js
@@ -0,0 +1,19 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "restrict",
+        description: "Exclude specific roles from Auto-Mod actions.",
+        options: [
+            {
+                type: 8,
+                name: "role",
+                description: "The role to exclude.",
+                required: true
+            }
+        ]
+    },
+    code: ``
+}
