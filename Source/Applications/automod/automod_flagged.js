8fd53ca2fb9ca31c5807985ea09d77e540f06317
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-22 15:02:18 +0100

    Create automod_flagged.js

---
+++ Source/Applications/automod/automod_flagged.js
@@ -0,0 +1,11 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "flagged",
+        description: "View all recently flagged messages in the server."
+    },
+    code: ``
+}
