ac9a8135d6a984b8d9a13df3328edfd64debd5aa
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-22 13:06:33 +0100

    Create status.js

---
+++ Source/Applications/automod/raid/status.js
@@ -0,0 +1,11 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "status",
+        description: "Check the current anti-raid status and settings."
+    },
+    code: ``
+}
