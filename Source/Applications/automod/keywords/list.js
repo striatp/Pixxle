d0caef0d2986aacb6d9a630faef4b35d8da0ca5b
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-22 12:34:18 +0100

    Create list.js

---
+++ Source/Applications/automod/keywords/list.js
@@ -0,0 +1,11 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "list",
+        description: "View all currently filtered keywords."
+    },
+    code: ``
+}
