7436051bcc61be1c6be3f30dfb8cd9f1ff869e79
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-23 12:09:59 +0100

    Update warning_records.js

--- Source/Applications/warning/warning_records.js
+++ Source/Applications/warning/warning_records.js
@@ -2,13 +2,19 @@ module.exports = {
     data: {
         type: 1,
         name: "records",
-        description: "Retrieve and manage warning records for members.",
+        description: "View the warning history of a user.",
         options: [
             {
                 type: 6, // User option type
-                name: "member",
-                description: "The member whose warning records you want to view.",
+                name: "target",
+                description: "The user whose warning history you want to check.",
                 required: true
+            },
+            {
+                type: 4,
+                name: "limit",
+                description: "The maximum number of warnings to display.",
+                required: false
             }
         ]
     },
