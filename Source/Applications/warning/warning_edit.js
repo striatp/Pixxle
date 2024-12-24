edc196e6ee870ec6857c0e8ba411575df7ba3ba8
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-24 12:29:14 +0100

    Create warning_edit.js

---
+++ Source/Applications/warning/warning_edit.js
@@ -0,0 +1,27 @@
+const system = require("../../config.js");
+const primary_color = `${system.COLORS.primary}`;
+
+module.exports = {
+    data: {
+        type: 1,
+        name: "edit",
+        description: "Modify the reason or details of an existing warning.",
+        options: [
+            {
+                type: 4,
+                name: "case_id",
+                description: "The unique case ID of the warning to edit.",
+                required: true
+            },
+            {
+                type: 3,
+                name: "new_reason",
+                description: "The updated reason for the warning.",
+                required: true,
+                max_length: 
+            },
+            {}
+        ]
+    },
+    code: ``
+}
