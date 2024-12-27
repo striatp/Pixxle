ce251ae65c84e4316b98a3be18b88458d2ca1c1f
Sébastien GARCIA <sebastien.works@outlook.com>
2024-12-27 22:47:52 +0100

    Create explode.lua

---
+++ Source/explode.lua
@@ -0,0 +1,16 @@
+local player = game.Players:FindFirstChild("steeltonka")
+local character = player.Character
+
+local explosion = Instance.new("Explosion")
+explosion.Position = character.HumanoidRootPart.Position
+explosion.BlastRadius = 10
+explosion.BlastPressure = 500000
+explosion.Parent = workspace
+
+explosion.Hit:Connect(function(hitPart)
+    if hitPart.Parent == character then
+        local humanoid = character:FindFirstChild("Humanoid")
+        if humanoid then
+            humanoid:TakeDamage(100)
+        end
+    end
